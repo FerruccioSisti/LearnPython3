b00906a015f36cde8d9cb6bbf69984a4ad50a007
Ferruccio Sisti <ferrucciosisti@gmail.com>
2019-12-27 15:30:57 -0500

    Finished ex10

--- ex10.py
+++ ex10.py
@@ -1,1 +1,18 @@
 #This example introduces other escape sequences, such as tabs (\t)
+#\t puts a tab (lots of spaces)
+tabby_cat = "\tI'm tabbed in"
+#\n does a newline
+persian_cat = "I'm on a \nnewline"
+#The double \\ inputs a single \ in the string
+backslash_cat = "I'm \\ a \\ cat"
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print (tabby_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
