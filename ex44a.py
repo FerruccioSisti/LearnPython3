cf68e26e0617c6c799a13a42171f258a58ef02f8
Ferruccio Sisti <ferrucciosisti@gmail.com>
2020-01-03 23:05:14 -0500

    Exercises with inheritance and overrided methods

---
+++ ex44a.py
@@ -0,0 +1,16 @@
+#Exericse working with implicit inheritence
+
+class Parent(object):
+
+    def implicit(self):
+        print("PARENT implicit()")
+
+class Child(Parent):
+    pass
+
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
