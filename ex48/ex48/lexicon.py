29c7a51af0f5d967ec49e827ab5028ee676e9c3c
Ferruccio Sisti <ferrucciosisti@gmail.com>
2020-01-07 18:19:20 -0500

    Got a working script but unit testing doesn't work

---
+++ ex48/ex48/lexicon.py
@@ -0,0 +1,50 @@
+import re
+
+#Define the lexicon tuples
+big_tuple = (("verb", "go"), ("direction", "north"), ("direction", "west"), ("direction", "east"), ("direction", "south"), ("verb", "kill"), ("verb", "eat"), ("stop", "the"), ("stop", "in"),
+("stop", "of"), ("noun", "bear"), ("noun", "princess"))
+# first_word = ("verb", "go")
+# second_word = ("direction", "north")
+# third_word = ("direction", "west")
+# fourth_word = ("direction", "east")
+# fifth_word = ("direction", "south")
+# sixth_word = ("verb", "kill")
+# seventh_word = ("verb", "eat")
+# eight_word = ("stop", "the")
+# ninth_word = ("stop", "in")
+# tenth_word = ("stop", "of")
+# eleventh_word = ("noun", "bear")
+# twelth_word = ("noun", "princess")
+# sentence = [first_word, second_word, third_word]
+
+def conver_number(s):
+    try:
+        return int(s)
+    except ValueError:
+        return None
+
+def scan(s):
+    dic = []
+
+    for word in s:
+        valid = True
+        if conver_number(word) != None:
+            numbers = ("number", word)
+            dic.append(numbers)
+            valid = False
+        else:
+            tuple_num = 0
+            for t in big_tuple:
+                if word in big_tuple[tuple_num]:
+                    dic.append(big_tuple[tuple_num])
+                    valid = False
+                tuple_num += 1
+
+        if valid:
+            errors = ("error", word)
+            dic.append(errors)
+    return dic
+
+stuff = input("> ")
+words = stuff.split()
+print(scan(words))
