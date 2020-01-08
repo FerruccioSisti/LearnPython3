ddbb420d7b152390c5f98febb169d26c6d19c7fe
Ferruccio Sisti <ferrucciosisti@gmail.com>
2020-01-07 20:24:18 -0500

    Removed ex48 and renamed ex48b to ex48 since it works completely

--- ex48/ex48/lexicon.py
+++ ex48/ex48/lexicon.py
@@ -1,50 +1,51 @@
-import re
+#This exercise is outputting a given sentence but adding the types of words used
 
-#Define the lexicon tuples
-big_tuple = (("verb", "go"), ("direction", "north"), ("direction", "west"), ("direction", "east"), ("direction", "south"), ("verb", "kill"), ("verb", "eat"), ("stop", "the"), ("stop", "in"),
-("stop", "of"), ("noun", "bear"), ("noun", "princess"))
-# first_word = ("verb", "go")
-# second_word = ("direction", "north")
-# third_word = ("direction", "west")
-# fourth_word = ("direction", "east")
-# fifth_word = ("direction", "south")
-# sixth_word = ("verb", "kill")
-# seventh_word = ("verb", "eat")
-# eight_word = ("stop", "the")
-# ninth_word = ("stop", "in")
-# tenth_word = ("stop", "of")
-# eleventh_word = ("noun", "bear")
-# twelth_word = ("noun", "princess")
-# sentence = [first_word, second_word, third_word]
+#Define the (TYPE, PAIR)'s
+big_tuple = (('direction', 'north'), ('direction', 'south'), ('direction', 'east'),
+            ('direction', 'west'), ('verb', 'go'), ('verb', 'kill'), ('verb', 'eat'),
+            ('stop', 'the'), ('stop', 'in'), ('stop', 'of'), ('noun', 'bear'),
+            ('noun', 'princess'))
 
-def conver_number(s):
+def scan(sentence):
+    '''
+    Function parses a sentence and then creates a dictionary to display the given
+    sentence as well as adding the type for each word
+    '''
+    words = sentence.split()
+    output = []
+
+    #Loop through each word in the sentence
+    for w in words:
+        i = 0
+        #Need this flag to check if word was not appended (if it needs 'error' type)
+        flg = True
+
+        #First check if its a number to avoid a needless loop
+        if convert_number(w) != None:
+            num = ("number", convert_number(w))
+            output.append(num)
+            flg = False
+            continue
+
+        #Iterate through the tuple list checking for a matching type
+        for t in big_tuple:
+            if w in big_tuple[i]:
+                output.append(big_tuple[i])
+                flg = False
+                break
+            i += 1
+
+        #If we don't have a definition or its not a number, its type is 'error'
+        if flg is True:
+            err = ("error", w)
+            output.append(err)
+    return output
+
+def convert_number(s):
+    '''
+    This method converts a string into an int (ONLY IF IT IS AN ACTUAL INT)
+    '''
     try:
         return int(s)
-    except ValueError:
+    except ValueError: #Return None if there are characters ^(0-9)
         return None
-
-def scan(words):
-    dic = []
-    s = words.split()
-
-    for word in s:
-        valid = True
-        if conver_number(word) != None:
-            numbers = ("number", word)
-            dic.append(numbers)
-            valid = False
-        else:
-            tuple_num = 0
-            for t in big_tuple:
-                if word in big_tuple[tuple_num]:
-                    dic.append(big_tuple[tuple_num])
-                    valid = False
-                tuple_num += 1
-
-        if valid:
-            errors = ("error", word)
-            dic.append(errors)
-    return dic
-
-stuff = input("> ")
-print(scan(stuff))
