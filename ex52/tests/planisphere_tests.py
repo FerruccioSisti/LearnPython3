1e96be428e8bfcbc80c81c92a753aa4eb9dbedad
Ferruccio Sisti <ferrucciosisti@gmail.com>
2020-01-16 22:39:04 -0500

    Started working on the app portion

--- ex52/tests/planisphere_tests.py
+++ ex52/tests/planisphere_tests.py
@@ -1,6 +1,6 @@
 #tester file for the project
 from nose.tools import *
-from gothonweb.planisphere import Room
+from gothonweb.planisphere import *
 
 def setup():
     print("SETUP!")
@@ -35,3 +35,14 @@ def test_map():
     assert_equal(start.go("west"), west)
     assert_equal(start.go("west").go("east"), start)
     assert_equal(start.go("down").go("up"), start)
+
+def test_gothon_game_map():
+    start_room = load_room(START)
+    assert_equal(start_room.go('shoot!'), generic_death)
+    assert_equal(start_room.go('dodge!'), generic_death)
+    room = start_room.go('tell a joke') assert_equal(room, laser_weapon_armory)
+
+
+
+
+    
